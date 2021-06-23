4336d6658397bc323ff6477324d72dac187e0e90
Joey Davidson <j.davidson18@gmail.com>
2021-06-23 09:44:43 -0600

    handlebars layouts complete

---
+++ Wishes/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({ extended:true}))
+app.use(express.json())
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Colorado23',
+    database: 'wishes_db',
+})
+
+connection.connect((err) => {
+    if (err) {
+      console.error(`error connecting: ${err.stack}`);
+      return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
+  });
+
+
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM wishes;', (err, data) => {
+        if (err) throw (err)
+
+        res.render('index', {wishes: data})
+    })
+
+
+})
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
