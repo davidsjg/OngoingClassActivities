bd73a96298ea738bc87f24cfc8a455b822ec90e2
Joey Davidson <j.davidson18@gmail.com>
2021-06-23 11:02:06 -0600

    get route working

--- watchList/server.js
+++ watchList/server.js
@@ -13,3 +13,34 @@ app.use(express.json());
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.use(express.static(path.join(__dirname, '/')));
 app.set('view engine', 'handlebars');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Colorado23',
+    database: 'moviePlanner_db',
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error(`error connecting: ${err.stack}`);
+      return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
+  });
+
+  app.get('/', (req, res) => {
+      connection.query('SELECT * FROM movies;', (err, data) => {
+          if (err) {
+              return res.status(500).end()
+          }
+
+          res.render('index', {movies: data})
+      })
+  })
+
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
