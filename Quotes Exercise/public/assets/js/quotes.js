d8ade3b1dba92ae816639596bfc5af26e2244b9b
Joey Davidson <j.davidson18@gmail.com>
2021-07-07 10:15:56 -0600

    update operation functioning

--- Quotes Exercise/public/assets/js/quotes.js
+++ Quotes Exercise/public/assets/js/quotes.js
@@ -11,8 +11,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 // CREATE QUOTE 
 const createQuoteBtn = document.getElementById('create-form')
-
-
+if (createQuoteBtn) {
     createQuoteBtn.addEventListener('submit', (e) => {
       e.preventDefault();
 
@@ -42,28 +41,11 @@ const createQuoteBtn = document.getElementById('create-form')
         location.reload();
       });
     });
-  
-
-
+}
 
 //DELETE
 const deleteBtn = document.querySelectorAll('.delQuote') 
 
-// deleteBtn.forEach((button) => {
-//     button.addEventListener('click', (e) => { 
-//         const id = e.target.getAttribute('data-id')
-//         console.log('delete quote id', id)
-//     })
-
-//     fetch(`api/quotes/${id}`, {
-//         method: 'DELETE'
-//     }).then((res) => {
-//         console.log(res)
-//         console.log(`Deleted ID: ${id}`)
-//         //reload the page
-//         location.reload()
-//     })
-// })
 deleteBtn.forEach((button) => {
     button.addEventListener('click', (e) => {
         console.log('button clicked')
@@ -83,4 +65,45 @@ deleteBtn.forEach((button) => {
     });
   });
 
-})
+
+
+ // UPDATE
+ const updateQuoteBtn = document.getElementById('update-form');
+
+ // Set up the event listener for the create button
+ if (updateQuoteBtn) {
+   updateQuoteBtn.addEventListener('submit', (e) => {
+     e.preventDefault();
+
+     // Grabs the id of the element that goes by the name, "id"
+     const id = e.target.getAttribute('data-id');
+
+     const updatedQuote = {
+       author: document.getElementById('auth').value.trim(),
+       quote: document.getElementById('quo').value.trim(),
+     };
+     console.log('updatedQuote', updatedQuote);
+
+     // Send POST request to create a new quote
+     fetch(`/api/quotes/${id}`, {
+       method: 'PUT',
+       headers: {
+         Accept: 'application/json',
+         'Content-Type': 'application/json',
+       },
+
+       // Make sure to serialize the JSON body
+       body: JSON.stringify(updatedQuote),
+     }).then((response) => {
+       // Check that the response is all good
+       // Reload the page so the user can see the new quote
+       if (response.ok) {
+         console.log(`Updated the quote with id: ${id}`);
+         location.assign('/');
+       } else {
+         alert('something went wrong!');
+       }
+     });
+   });
+ }
+});
--- Quotes Exercise/server.js
+++ Quotes Exercise/server.js
@@ -79,6 +79,40 @@ app.delete('/api/quotes/:id', (req, res) => {
   );
 });
 
+// Update a quote by an id and then redirect to the root route.
+app.put('/api/quotes/:id', (req, res) => {
+  connection.query(
+    'UPDATE quotes SET author = ?, quote = ? WHERE id = ?',
+    [req.body.author, req.body.quote, req.params.id],
+    (err, result) => {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      }
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
+
+app.get('/:id', (req, res) => {
+  connection.query(
+    'SELECT * FROM quotes where id = ?',
+    [req.params.id],
+    (err, data) => {
+      if (err) {
+        return res.status(500).end();
+      }
+
+      console.log(data);
+      res.render('quotes', data[0]);
+    }
+  );
+});
+
 
 app.listen(PORT, () =>
   console.log(`Server listening on: http://localhost:${PORT}`)
