b9cca2c70fa71037a8f450a275b96c870c9bd107
Joey Davidson <j.davidson18@gmail.com>
2021-07-17 11:35:35 -0600

    handlebars front end established blog

--- Blog/server.js
+++ Blog/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expbs = require('express-handlebars');
 
 // Sets up the Express App
 const app = express();
@@ -7,12 +8,19 @@ const PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 const db = require('./models')
 
+var compression = require('compression')
+
 // Sets up the Express app to handle data parsing 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(compression())
 
 //static director 
 app.use(express.static('public'))
+
+//handlebars
+app.engine('handlebars', expbs ({ defaultLayout: "main", partialsDir: __dirname + './public/views'}));
+app.set('view engine', 'handlebars');
 
 //routes
 require('./routes/api-routes.js')(app)
