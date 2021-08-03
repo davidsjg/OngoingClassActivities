161fc02f4ac9762a114a0086db48429b253626a4
Joey Davidson <j.davidson18@gmail.com>
2021-08-03 09:48:00 -0600

    state now functioning

--- react-class/src/App.js
+++ react-class/src/App.js
@@ -9,24 +9,14 @@ import friends from './friends.json';
 //convert from functional based to class based, use the state object, create removeFriend method, call FriendCard one time, passing down props as diff values in that object nested inside the array.  also, we are going to pass down the removeFriend function.  pass down props and map over 
 
 export default class FriendState extends React.Component {
-
   state = {
     friends
   }
 
-
-handleIncrement = () => {
-  this.setState({friends: this.dispFriends + 1})
-}
-
 removeFriend = (id) => {
-
   const friends = this.state.friends.filter((friend) => friend.id !== id)
-
   this.setState({friends})
 }
-
-//take in the id of the friend we want to remove(hide)
 
 render() {
   return (
