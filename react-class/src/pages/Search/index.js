40c7804d68e279f83000b5f2eae5965d7d31834e
Joey Davidson <j.davidson18@gmail.com>
2021-08-16 15:42:09 -0600

    successfully delayed API call via debounce Custom Hook

--- react-class/src/pages/Search/index.js
+++ react-class/src/pages/Search/index.js
@@ -1,10 +1,10 @@
-import React, { Component, useEffect, useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
 import Container from "../../components/Container";
 import SearchForm from "../../components/SearchForm";
 import SearchResults from "../../components/SearchResults";
 import Alert from "../../components/Alert";
+import useDebounce from "../../utils/debounceHook";
 
 function Search() {
   const [search, setSearch] = useState("Wikipedia");
@@ -12,62 +12,44 @@ function Search() {
   const [url, setUrl] = useState("");
   const [error, setError] = useState("");
 
-  // When the component mounts, update the title to be Wikipedia Searcher
+  const debouncedSearchTerm = useDebounce(search, 500);
+
   useEffect(() => {
-    document.title = "Wikipedia Searcher";
+    if (!search) {
+      return;
+    }
 
-    API.searchTerms(this.state.search)
-      .then((res) => {
-        if (res.data.length === 0) {
-          throw new Error("No results found.");
-        }
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
+    if (debouncedSearchTerm) {
+      API.searchTerms(search)
+        .then(res => {
+          if (res.data.length === 0) {
+            throw new Error("No results found.");
+          }
+          if (res.data.status === "error") {
+            throw new Error(res.data.message);
+          }
+          setTitle(res.data[1][0]);
+          setUrl(res.data[3][0]);
+        })
+        .catch(err => setError(err));
+    }
+  }, [debouncedSearchTerm]);
 
-        setTitle(res.data[1][0]);
-        setUrl(res.data[3][0]);
-      })
-      .catch((err) => setError(err));
-  });
-
-  const handleInputChange = (event) => {
+  const handleInputChange = event => {
     setSearch(event.target.value);
   };
-
-  // const handleFormSubmit = (event) => {
-  //   event.preventDefault();
-  //   if (!this.state.search) {
-  //     return;
-  //   }
-  //   API.searchTerms(this.state.search)
-  //     .then((res) => {
-  //       if (res.data.length === 0) {
-  //         throw new Error("No results found.");
-  //       }
-  //       if (res.data.status === "error") {
-  //         throw new Error(res.data.message);
-  //       }
-  //       this.setState({
-  //         title: res.data[1],
-  //         url: res.data[3][0],
-  //         error: "",
-  //       });
-  //     })
-  //     .catch((err) => this.setState({ error: err.message }));
-  // };
 
   return (
     <div>
       <Container style={{ minHeight: "100vh" }}>
         <h1 className="text-center">Search For Anything on Wikipedia</h1>
-        <Alert
-          type="danger"
-          style={{ opacity: error ? 1 : 0, marginBottom: 10 }}
-        >
+        <Alert type="danger" style={{ opacity: error ? 1 : 0, marginBottom: 10 }}>
           {error}
         </Alert>
-        <SearchForm handleInputChange={handleInputChange} results={search} />
+        <SearchForm
+          handleInputChange={handleInputChange}
+          results={search}
+        />
         <SearchResults title={title} url={url} />
       </Container>
     </div>
