73872697df61ea2399db392b42583c4563db45d8
Joey Davidson <j.davidson18@gmail.com>
2021-08-19 10:16:57 -0600

    useReducer and useRef functioning properly

--- react-class/src/App.js
+++ react-class/src/App.js
@@ -1,57 +1,61 @@
-import React, { useReducer } from "react";
+import userEvent from "@testing-library/user-event";
+import React, { useReducer, useRef } from "react";
+import "./App.css";
 
-function Count() {
-  const dogs = [
-    {
-      name: "Harry",
-      image: "https://images.dog.ceo/breeds/vizsla/n02100583_10960.jpg",
-    },
-    {
-      name: "Hermione",
-      image: "https://images.dog.ceo/breeds/husky/n02110185_1511.jpg",
-    },
-  ];
+const TodoList = () => {
+  // Placeholder array
 
-  const [count, dispatch] = useReducer(
-    (state, action) => {
-      switch (action) {
-        case "praiseHarry":
-          return { ...state, HarryPraises: state.HarryPraises + 1 };
+  const inputRef = useRef();
 
-        case "praiseHermione":
-          return { ...state, HermionePraises: state.HermionePraises + 1 };
+  const [items, dispatch] = useReducer((state, action) => {
+    ///state will be our array list of things to do
+    switch (action.type) {
+      case "add":
+        return [
+          ...state,
+          {
+            id: state.length * Math.random(),
+            name: action.name,
+          },
+        ];
+      default:
+        return state;
+    }
+  }, []);
 
-        default:
-          return state;
-      }
-    },
-    { HarryPraises: 0, HermionePraises: 0 }
-  );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch({
+      type: "add",
+      name: inputRef.current.value,
+    });
+    inputRef.current.value = "";
+  };
 
   return (
-    <div className="App">
-      <div className="row mt-5">
-        {dogs.map((item) => (
-          <div key={item.name} className="card mx-auto col-4">
-            <img className="card-img-top" src={item.image} alt={item.name} />
-            <div className="card-body">
-              <h4 className="card-title">{item.name}</h4>
-              <p className="card-text">
-                {item.name} has been praised {count[item.name + "Praises"]}{" "}
-                times!
-              </p>
-              <button
-                className="btn btn-primary"
-                onClick={() => dispatch("praise" + item.name)}
-              >
-                Praise
-              </button>
-            </div>
-          </div>
+    <div className="container text-center">
+      <h1>Create a Todo List!</h1>
+      <form className="form-group mt-5" onSubmit={handleSubmit}>
+        <input
+          ref={inputRef}
+          className="form-control"
+          placeholder="Start typing what you need to do..."
+        />
+        <button className="btn btn-success mt-3 mb-5" type="submit">
+          Add to List
+        </button>
+      </form>
+      <h4>My Todo List:</h4>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item.id}>
+            {item.name}{" "}
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
-}
+};
 
-export default Count;
+export default TodoList;
